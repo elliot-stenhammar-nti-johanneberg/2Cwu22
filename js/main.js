0084da72c2dedfcbc8a83e46c0950f7537ead050
elliot-stenhammar-nti-johanneberg <elliot.markstromstenhammar@elev.ga.ntig.se>
2022-03-15 14:45:53 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -4,20 +4,21 @@ const activeNavBtn = document.getElementById(path)
 activeNavBtn.classList.add("current");
 
 // Mobile side-nav open & close
+const menu = document.getElementById("side-menu")
+const overlay = document.getElementById("overlay")
 const menuBtnOpen = document.getElementById("menu-btn")
+const menuBtnClose = document.getElementById("menu-close-btn")
+
 menuBtnOpen.addEventListener("click", () => {
     menu.classList.toggle("side-menu-open")
     overlay.classList.toggle("overlay-open")
 })
 
-const menuBtnClose = document.getElementById("menu-close-btn")
 menuBtnClose.addEventListener("click", () => {
     menu.classList.toggle("side-menu-open")
     overlay.classList.toggle("overlay-open")
 })
 
-const overlay = document.getElementById("overlay")
-const menu = document.getElementById("side-menu")
 overlay.addEventListener("click", () => {
     menu.classList.toggle("side-menu-open")
     overlay.classList.toggle("overlay-open")
